8b337aef93a4c1a7a4fbfd3e89dd89baedbc3a2d
Nicolas-Chourot <Nicolas.Chourot@clg.qc.ca>
2023-04-01 18:17:17 -0400

    New Data

--- Models/DB.cs
+++ Models/DB.cs
@@ -12,7 +12,14 @@ namespace MDB.Models
 {
     public class DB
     {
+        #region singleton setup
         private static readonly DB instance = new DB();
+        public static DB Instance
+        {
+            get { return instance; }
+        }
+        #endregion
+        #region Repositories
         public static Repository<Gender> Genders { get; set; }
         public static Repository<UserType> UserTypes { get; set; }
         public static Repository<UnverifiedEmail> UnverifiedEmails { get; set; }
@@ -24,6 +31,8 @@ namespace MDB.Models
         public static DistributorsRepository Distributors { get; set; }
         public static CastingsRepository Castings { get; set; }
         public static DistributionsRepository Distributions { get; set; }
+        #endregion
+        #region initialization
         public DB()
         {
             Genders = new Repository<Gender>();
@@ -38,23 +47,20 @@ namespace MDB.Models
             Castings = new CastingsRepository();
             Distributions = new DistributionsRepository();
             InitRepositories(this);
-        }
-        public static DB Instance
-        {
-            get { return instance; }
-        }
-        public static void InitRepositories(DB db)
+        }     
+        private static void InitRepositories(DB db)
         {
             string serverPath = HostingEnvironment.MapPath(@"~/App_Data/");
             PropertyInfo[] myPropertyInfo = db.GetType().GetProperties();
             foreach(PropertyInfo propertyInfo in myPropertyInfo)
             {
-                if (propertyInfo.Name != "Instance")
+                if (propertyInfo.PropertyType.Name.Contains("Repository"))
                 {
                     MethodInfo method = propertyInfo.PropertyType.GetMethod("Init");
                     method.Invoke(propertyInfo.GetValue(db), new object[] { serverPath + propertyInfo.Name + ".json" });
                 }
             }
         }
+        #endregion
     }
 }
